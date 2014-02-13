f08727148789a2aa286fd5c1a43353b9139a788f
Anthonny James Crespo Colmenares <an_crespo_col@hotmail.com>
2014-02-13 16:32:28 -0600

    Cambios en la interfaz inicial, y cambiadas las rutas de contacto para la descarga de informacion by Anthonny James Crespo Colmenares (an_crespo_col@hotmail.com).

--- sunday/js/base.js
+++ sunday/js/base.js
@@ -14,10 +14,75 @@ function nullHandler() {
 }
 
 function createDB(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS userData (version integer, language integer, id varchar[40], email varchar[255]);');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS userData (version integer, language integer);');
     tx.executeSql('CREATE TABLE IF NOT EXISTS blessed_week (week_id integer, mon TEXT, tue TEXT, wed TEXT, thu TEXT, fri TEXT, sat TEXT, sun TEXT);');
     tx.executeSql('CREATE TABLE IF NOT EXISTS content_outline (week_id integer, out int, content TEXT);');
     tx.executeSql('CREATE TABLE IF NOT EXISTS lessons (week integer, title varchar[50], out1 varchar[50], out2 varchar[50], out3 varchar[50], out4 varchar[50], out5 varchar[50], out6 varchar[50], out7 varchar[50], out8 varchar[50], question1 varchar[255], question2 varchar[255], conclusion text, verse varchar[50], memory_verse text, date long, intro TEXT, bible_pass TEXT, numberOutLines INTEGER);');
     tx.executeSql('CREATE TABLE IF NOT EXISTS note (note_id integer PRIMARY KEY, date varchar[18],title varchar[27], content TEXT);');
     queryClient();
 }
+
+var publicKey='FrfuSF7Hnuf7';
+var privateKey='YbeusYXc60c1V1';
+
+function makeRandomKey()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function InsertLessons(tx){
+	var countLesson = 0;
+	
+    $.each(dataWS, function(k,v){
+			
+            if(v.content.length == 2){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.introduction+'","'+v.bible_passage+'", 2)';    
+            }
+            if(v.content.length == 3){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, out3, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.outline[2]+'","'+v.introduction+'","'+v.bible_passage+'", 3)';    
+            }
+            if(v.content.length == 4){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, out3, out4, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.outline[2]+'","'+v.outline[3]+'","'+v.introduction+'","'+v.bible_passage+'", 4)';    
+            }
+            if(v.content.length == 5){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, out3, out4, out5, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.outline[2]+'","'+v.outline[3]+'","'+v.outline[4]+'","'+v.introduction+'","'+v.bible_passage+'", 5)';    
+            }
+            if(v.content.length == 6){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, out3, out4, out5, out6, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.outline[2]+'","'+v.outline[3]+'","'+v.outline[4]+'","'+v.outline[5]+'","'+v.introduction+'","'+v.bible_passage+'", 6)';    
+            }
+            if(v.content.length == 7){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, out3, out4, out5, out6, out7, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.outline[2]+'","'+v.outline[3]+'","'+v.outline[4]+'","'+v.outline[5]+'","'+v.outline[6]+'","'+v.introduction+'","'+v.bible_passage+'", 7)';    
+            }
+            if(v.content.length == 8){
+                sqlLesson= 'INSERT OR REPLACE INTO lessons (week, title, date, memory_verse, verse, conclusion, question1, question2, out1, out2, out3, out4, out5, out6, out7, out8, intro, bible_pass, numberOutLines) VALUES ('+v.week+',"'+ v.title+'","'+v.date+'","'+v.memory_verse+'","'+v.verse+'","'+v.conclusion+'","'+v.questions[0]+'","'+v.questions[1]+'","'+v.outline[0]+'","'+v.outline[1]+'","'+v.outline[2]+'","'+v.outline[3]+'","'+v.outline[4]+'","'+v.outline[5]+'","'+v.outline[6]+'","'+v.outline[7]+'","'+v.introduction+'","'+v.bible_passage+'", 8)';    
+            }
+            
+			sqlBlessedWeek = 'INSERT OR REPLACE INTO blessed_week(week_id, mon, tue, wed, thu, fri, sat, sun) VALUES('+v.week+',"'+v.blessed_week[0]+'","'+v.blessed_week[1]+'","'+v.blessed_week[2]+'","'+v.blessed_week[3]+'","'+v.blessed_week[4]+'","'+v.blessed_week[5]+'","'+v.blessed_week[6]+'" )';
+			
+            tx.executeSql(sqlBlessedWeek);
+        
+            //Insertar cuando sea version pro
+			/*for(var i=0; i<v.content.length; i++){
+			    for(var j=0; j<v.content[i].length; j++){
+					sqlContenOutline = 'INSERT INTO content_outline(week_id, out, content) VALUES('+v.week+","+(i+1)+',"'+v.content[i][j]+'")';
+					tx.executeSql(sqlContenOutline);		
+				}
+			}*/
+			
+			tx.executeSql(sqlLesson);
+			countLesson++;
+            
+		});
+		
+		$.mobile.loading( 'hide' );
+		console.log("number of lessons "+countLesson);
+        insertClient(0);    
+        //eventsHome();
+}
+
--- sunday/js/registration.js
+++ sunday/js/registration.js
@@ -2,6 +2,7 @@ var db;
 var dataWS = '';
 var idDispositivo = '';
 var Online = '';
+var dataWS = '';
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
@@ -18,12 +19,6 @@ function init() {
     
     $('#language').on('change', function(event) {
         if ($('#language').val() != -1) {
-            $("#labelName").text(language[$(this).val()].name);
-            $("#name").attr('placeholder', language[$(this).val()].name);
-            $("#labelEmail").text(language[$(this).val()].email);
-            $("#email").attr('placeholder', language[$(this).val()].email);
-            $("#labelPhone").text(language[$(this).val()].phoneNumber);
-            $("#phoneNumber").attr('placeholder', language[$(this).val()].phoneNumber);
             $("#labelCode").text(language[$(this).val()].codeActivation);
             $("#codeActivation").attr('placeholder', language[$(this).val()].codeActivation);
             $("#button .ui-btn-text").text(language[$(this).val()].button);
@@ -35,28 +30,21 @@ function init() {
 
     $("#button").on('tap', function(event) {
         event.preventDefault();
-        if ($("#name").val() == "" || $("#email").val() == "" || $("#phoneNumber").val() == "" || $('#region').val() === -1) {
-            alert("" + language[$('#language').val()].messageRequered);
-        }else{
-            if (!emailValidation($("#email").val())) {
-                alert(language[$('#language').val()].messageEmail);
+       
+        if(Online){
+            if($("#codeActivation").val() != ""){
+                APIRequestCode();
             }else{
-                if($("#email").val() != $("#email2").val()){
-                    alert(language[$('#language').val()].sameEmail)
-                }
-                else{
-                    if(Online){
-                        if($("#codeActivation").val() != ""){
-                            APIRequestCode();
-                        }else{
-                            APIResquestEmail();
-                        }
-                    }else{
-                        alert(language[$('#language').val()].noInternet);
-                    }
-                }
+                var randomKey = makeRandomKey();
+                var hash = CryptoJS.SHA1(publicKey+privateKey+'findAllLessonsByLanguageGET'+randomKey);
+                APILessonsByLenguage(hash, randomKey);
             }
+        }else{
+            alert(language[$('#language').val()].noInternet);
         }
+                
+            
+        
     });
 }
 
@@ -67,7 +55,7 @@ function existentUser() {
 
 function successInsert() {
     alert(language[$('#language').val()].onSuccess);
-    window.location.href = "home.html";
+    //window.location.href = "home.html";
     //$.mobile.changePage( "home.html", { transition: "flip", reloadPage: true });
 }
 
@@ -101,7 +89,7 @@ function queryClient(){
         transaction.executeSql(sql, [],function(transaction, result) {
                     if (result.rows.length > 0) {
                         console.log("hay un cliente registrado");                      
-                        window.location.href = "home.html";
+                        //window.location.href = "home.html";
                     }
                     else{
                         console.log("NO hay un cliente registrado");
@@ -120,15 +108,6 @@ function insertClient(version) {
     });
     
 }
-
-function emailValidation(value) {
-    if (/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/.test(value)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 function APIRequestCode() {
     var url = "http://dry-dawn-9293.herokuapp.com/user/active_code/" + $("#codeActivation").val();
     $.ajaxSetup({
@@ -164,34 +143,9 @@ function APIRequestCode() {
     });
 }
 
-function APIRequestUpdateCount(idUser, count) {
+function APILessonsByLenguage(hash, randomKey) {
 
-    var url = "http://dry-dawn-9293.herokuapp.com/user/" + idUser + "/" + count;
-    console.log("contador a actualizar es " + count);
-
-    $.ajaxSetup({
-        contentType: "application/json; charset=utf-8",
-        type: "PUT",
-        dataType: "json",
-        url: url
-    });
-
-    $.ajax({
-        data: JSON.stringify({"count_device": count, "uuid": idDispositivo}),
-        success: function(data) {
-            alert(language[$('#language').val()].successUpdatePro);
-            dataWS = data;
-            insertClient(1);
-        },
-        error: function(xhr, status, error) {
-            alert(language[$('#language').val()].errorUpdatePro); 
-        }
-    });
-}
-
-function APIResquestEmail() {
-
-    var url = "http://dry-dawn-9293.herokuapp.com/user/email/" + $("#email").val();
+    var url = "http://sunday.galeriasmall.com/api/lessons/language/"+$('#language').val()+"?accessKey="+hash+"&randomKey="+randomKey;
     $.ajaxSetup({
         type: "GET",
         dataType: "json",
@@ -200,36 +154,45 @@ function APIResquestEmail() {
 
     $.ajax({
         success: function(data) {
-            console.log(data);
-            alert(language[$('#language').val()].userRegistrated);
-            dataWS = data;
-            insertClient(0);// Usuario free
+            console.log(data.status);
+            if(data.status == '200'){
+                //Hago al insersion de las lecciones    
+                //alert('Guardar');
+                dataWS = data.data;
+                db = openDatabase("sundayApp", "1.0", "Sunday School DB", 1000000);
+                $.mobile.loading( 'show', {
+    				text: "Downloading data",
+    				textVisible: "false",
+    				theme: "b",
+    				html: ""
+    			});
+                db.transaction(InsertLessons, errorCB, successCB);
+            }
+            if(data.message == 'no_data'){
+                alert('No hay lecciones para este idioma, intenta con otro por favor');    
+            }
+            
+            if(data.message == 'accessk_key_invalid' || data.message == 'no_paramenters'){
+                alert('Peticion invalida');        
+            }
+            
+            //alert(language[$('#language').val()].userRegistrated);
+            //dataWS = data;
+            //insertClient(0);// Usuario free
         },
         error: function(xhr, status, error) {
-            console.log("usuario nuevo");
-            APIRequestInsert();
+            console.log("error en el servidor");
+            
         }
     });
+    
 }
 
-function APIRequestInsert() {
+function insertClient(version) {
+	db = openDatabase("sundayApp", "1.0", "Sunday School DB", 1000000);
+    db.transaction(function(transaction) {
+        transaction.executeSql('INSERT INTO userData (version, language) VALUES (?,?)',
+               [version, $('#language').val()], successInsert, errorHandler);
+    });
     
-    var url = "http://dry-dawn-9293.herokuapp.com/user";
-    $.ajaxSetup({
-        contentType: "application/json; charset=utf-8",
-        type: "POSt",
-        dataType: "json",
-        url: url
-    });
-
-    $.ajax({
-        data: JSON.stringify({"plataform": device.platform, name: $("#name").val(), email: $("#email").val(), phone_number: $("#phoneNumber").val(), "version": 0, "active_code": "none", "language": $('#language').val()}),
-        success: function(data) {
-            dataWS = data;
-            insertClient(0); // Usuario free
-        },
-        error: function(xhr, status, error) {
-            alert(language[$('#language').val()].onError);
-        }
-    });
 }
